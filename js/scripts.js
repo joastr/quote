ae9ab273e61d259f423be13a8ab833f006eb1bb1
Joanna Kasperek <joastr1@gmail.com>
2018-12-06 12:13:24 +0100

    add promise

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,55 @@
 // plik scripts.js
 
 var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var prefix = "https://cors-anywhere.herokuapp.com/";
 
+
+function getQuote() {
+    fetch(prefix + quoteUrl, { cache: "no-store" })
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(createTweet);
+}
+
+function createTweet(input) {
+    var data = input[0];
+
+    var dataElement = document.createElement('div');
+    dataElement.innerHTML = data.content;
+    var quoteText = dataElement.innerText.trim();
+	var quoteAuthor = data.title;
+	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) {
+		getQuote();
+	} else {
+		var tweet = tweetLink + encodeURIComponent(tweetText);
+		document.querySelector('.quote').innerText = quoteText;
+		document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+		document.querySelector('.tweet').setAttribute('href', tweet);
+	}
+	
+	
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    getQuote();
+    document.querySelector('.trigger').addEventListener('click', function() {
+        getQuote();
+    });
+});
+
+
+
+
+
+
+
+
+
+/*---------------------JQUERY-------------------------
 function getQuote() {
 	$.getJSON(quoteUrl, createTweet);
 }
@@ -31,3 +78,5 @@ $(document).ready(function() {
 		getQuote();
 	})
 });
+
+*/
